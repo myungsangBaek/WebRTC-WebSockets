f6ae74c66405f9beb6c760dcff75bc3056955178
myungsangBaek <bms6168@naver.com>
2022-03-27 21:01:04 +0900

    [FEATURE] Generate chat completed

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,6 @@
+const ul = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
 //서버로의 연결
 const socket = new WebSocket(`ws://${window.location.host}`);
 
@@ -13,6 +16,15 @@ socket.addEventListener("close", () => {
   console.log("Disconnected from Server");
 });
 
-setTimeout(() => {
-  socket.send("hello from the browser!");
-}, 10000);
+// setTimeout(() => {
+//   socket.send("hello from the browser!");
+// }, 10000);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -16,14 +16,16 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 //연결된 브라우저
 wss.on("connection", (socket) => {
+  sockets.push(socket);
   console.log("Connected to Browser");
   socket.on("close", () => console.log("Disconnected from the Browser"));
   socket.on("message", (message) => {
-    console.log(message.toString("utf-8"));
+    sockets.forEach((aSocket) => aSocket.send(message.toString("utf-8")));
   });
-  socket.send("hello!");
 });
 
 server.listen(3000, handleListen);
