49c965b556cd1c5e54903966e6f3d45f5bbf3159
myungsangBaek <bms6168@naver.com>
2022-04-04 23:51:39 +0900

    [FEATURE] Generate parse json

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,12 +1,19 @@
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
 
 //서버로의 연결
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener("open", () => {
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
+function handleOpen() {
   console.log("Connected to Server");
-});
+}
+socket.addEventListener("open", handleOpen);
 
 socket.addEventListener("message", (message) => {
   console.log("New message: ", message.data);
@@ -26,8 +33,15 @@ socket.addEventListener("close", () => {
 function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
   input.value = "";
 }
 
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
